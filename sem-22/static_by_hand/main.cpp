96c428b727231d0b4ad2ab9a8ec52d0d260fa35e
Stepan Styopkin <smstyopkin@edu.hse.ru>
2025-03-09 15:05:13 +0000

    Cmake cmake cmake

---
+++ sem-22/static_by_hand/main.cpp
@@ -0,0 +1,7 @@
+#include "math_utils.h"
+#include <iostream>
+
+int main() {
+    std::cout << "5 + 3 = " << add(5, 3) << std::endl;
+    return 0;
+}
---
+++ sem-22/static_by_hand/math_utils.h
@@ -0,0 +1,15 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // MATH_UTILS_H
